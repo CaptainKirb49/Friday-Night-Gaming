461479af000958a6cf36602fa5362e2457a16ec3
CaptainKirb <80009304+CaptainKirb49@users.noreply.github.com>
2021-10-03 15:01:44 -0600

    Initial Push
    Most of the mod all in one push

---
+++ Friday Night Gaming Ver 2/example_mods/data/shantie/script.lua
@@ -0,0 +1,24 @@
+local countDownsToBlock = 2;
+function onStartCountdown()
+    if countDownsToBlock > 0 and isStoryMode and not seenCutscene then --Block the first 2 countdowns
+        setProperty('inCutscene', true);
+        if countDownsToBlock == 2 then
+            startVideo('LukeStart');
+            seenCutscene = false;
+        elseif countDownsToBlock == 1 then
+            runTimer('startDialogue', 0.8);
+            seenCutscene = true;
+        end
+        countDownsToBlock = countDownsToBlock - 2;
+        return Function_Stop;
+    end
+    return Function_Continue;
+end
+
+function onTimerCompleted(tag, loops, loopsLeft)
+    if tag == 'startDialogue' then
+        print("Trying to start dialogue...");
+        startDialogue('machine-dialogue', 'dialogueCalm');
+        print("dialogue ended");
+    end
+end
