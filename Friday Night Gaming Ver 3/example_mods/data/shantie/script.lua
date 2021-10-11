e4839a4091fa085e4bb8c4ebd686f06c7398bdab
moloki52 <supwinner52@gmail.com>
2021-10-11 16:48:20 -0400

    V3
    poggers

---
+++ Friday Night Gaming Ver 3/example_mods/data/shantie/script.lua
@@ -0,0 +1,24 @@
+local countDownsToBlock = 1;
+function onStartCountdown()
+    if countDownsToBlock > 0 and isStoryMode and not seenCutscene then --Block the first 2 countdowns
+        setProperty('inCutscene', true);
+        if countDownsToBlock == 2 then
+            startVideo('cutscene1');
+            seenCutscene = false;
+        elseif countDownsToBlock == 1 then
+            runTimer('startDialogue', 0.8);
+            seenCutscene = true;
+        end
+        countDownsToBlock = countDownsToBlock - 1;
+        return Function_Stop;
+    end
+    return Function_Continue;
+end
+
+function onTimerCompleted(tag, loops, loopsLeft)
+    if tag == 'startDialogue' then
+        print("Trying to start dialogue...");
+        startDialogue('shantie', 'shantie');
+        print("dialogue ended");
+    end
+end
